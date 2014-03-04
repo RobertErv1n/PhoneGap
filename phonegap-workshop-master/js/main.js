4ba9a43b2da889113daddd82a4f1a27353e31e42
RobertErv1n <robert.ervin16@gmail.com>
2014-03-03 21:16:35 -0500

    3

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -14,19 +14,19 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore(function() {
-			this.showAlert('Store Initialized', 'Info');
+		var self = this;
+		this.store = new LocalStorageStore (function() {
+		self.showAlert('Store Initialized', 'Info'); 
 		});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
 
-};
-	showAlert: function(message, title) {
-		if(navigator.notification) {
+	showAlert: function (message, title) {
+		if (navigator.notification) {
 			navigator.notification.alert(message, null, title, 'OK');
 		} else {
-			alert(title ? (title + ": " + message) : message;
-			}
-	};
-
+			alert(title ? (title + ": " + message) : message);
+		}
+	}
+};
 app.initialize();
